73eeae1626a29d89f57e66674dc16b9210e58c54
Ludvig Jansson <revprivateg@gmail.com>
2021-10-13 12:38:32 +0200

    Major changes to the API part of the code and minor styling changes.

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
-var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#search").val();
-$.ajax({
-url: flickerAPI,
-dataType: "jsonp", // jsonp
-jsonpCallback: 'jsonFlickrFeed', // add this property
-success: function (result, status, xhr) {
-$.each(result.items, function (i, item) {
-$("<img>").attr("src", item.media.m).appendTo("#outputDiv");
-if (i === 5) {
-return false;
-}
-});
-},
-error: function (xhr, status, error) {
-console.log(xhr)
-$("#outputDiv").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-}
-});
+var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#search").val() + '&jsoncallback=1';
 
-var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=";
-$.getJSON(flickerAPI, {
-tags: $("#search").val(),
-tagmode: "any",
-format: "json"
-}).done(function (result, status, xhr) {
-$.each(result.items, function (i, item) {
-$("<img>").attr("src", item.media.m).appendTo("#outputDiv");
-if (i === 5) {
-return false;
-}
-});
-}).fail(function (xhr, status, error) {
-alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-});
+
+// $.getJSON(flickerAPI, {
+//     tags: $("#search").val(),
+//     tagmode: "any",
+//     format: "json"
+// }).done(function (result, status, xhr) {
+//     $.each(result.items, function (i, item) {
+//         $("<img>").attr("src", item.media.m).appendTo("#outputDiv").addClass('image-flick');
+//         if (i === 5) { 
+//             return false;
+//         }
+//     });
+
+// });
 
 function JavaScriptFetch() {
     var script = document.createElement('script');
-    script.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + document.getElementById("search").value;;
+    script.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + document.getElementById("search").value;
     document.querySelector('head').appendChild(script);
-    }
-    function jsonFlickrFeed(data) {
+    // $.getJSON(flickerAPI, {
+    //     tags: $("#search").val(),
+    //     tagmode: "any",
+    //     format: "json"
+    // }).done(function (result, status, xhr) {
+    //     $.each(result.items, function (i, item) {
+    //         $("<img>").attr("src", item.media.m).appendTo("#outputDiv").addClass('image-flick');
+    //         if (i === 5) { 
+    //             return false;
+    //         }
+    //     });
+    
+    // });
+}
+function jsonFlickrFeed(data) {
     var image = "";
     data.items.forEach(function (element) {
-    image += "<img src=\"" + element.media.m + "\"/>";
+        image += "<img src=\"" + element.media.m + "\"/>";
     });
     document.getElementById("outputDiv").innerHTML = image;
-    }
+}
+
+// To make the search button work upon pressing enter: //
+
+var input = document.getElementById("search");
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {                     // Number 13 is the "Enter" key on the keyboard
+    event.preventDefault();
+    document.getElementById("submit").click();
+  }
+});
