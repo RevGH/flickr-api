5769a14e8d187b31ee8f7fa48e28d904975901c4
Ludvig Jansson <revprivateg@gmail.com>
2021-10-15 15:26:49 +0200

    Fixed a few comments

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------ Connecting to the API & fetches then outputs images ------------------------------------------------------------------
+// ------------------------------------------------------------------ Connects to the API & fetches, then outputs images ------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#search").val() + '&jsoncallback=1';
+// Callback connected to #search
+var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#search").val() + '&jsoncallback=1';      // Flickr public 'uploads from everyone'
 
-function JavaScriptFetch() {
+// Fetch //
+function JavaScriptFetch() {                                                                                                                
     var script = document.createElement('script');
     script.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + document.getElementById("search").value;
     document.querySelector('head').appendChild(script);
 }
 
-function jsonFlickrFeed(data) {
+// Output
+function jsonFlickrFeed(data) {                                                                                                            
     var image = "";
     data.items.forEach(function (element) {
         image += "<img src=\"" + element.media.m + "\"/>";
     });
-    document.getElementById("outPutDiv").innerHTML = image;
+    document.getElementById("outPutDiv").innerHTML = image;           // outPutDiv = image
 }
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -26,7 +29,7 @@ function jsonFlickrFeed(data) {
 var input = document.getElementById("search");
 
 input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {                     // Number 13 = 'ENTER' key on the keyboard
+  if (event.keyCode === 13) {                      // Number 13 = 'ENTER' key on the keyboard
     event.preventDefault();
     document.getElementById("submit").click();
   }
