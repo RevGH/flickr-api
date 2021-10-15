723a5d11af381e5b53b41a384d329737f3e7b323
Ludvig Jansson <revprivateg@gmail.com>
2021-10-15 12:04:20 +0200

    Final commit before hand-in. Changed a lot of styling and added comments to the code to make it more accessible.

--- app.js
+++ app.js
@@ -1,53 +1,34 @@
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------ Connecting to the API & fetches then outputs images ------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#search").val() + '&jsoncallback=1';
-
-
-// $.getJSON(flickerAPI, {
-//     tags: $("#search").val(),
-//     tagmode: "any",
-//     format: "json"
-// }).done(function (result, status, xhr) {
-//     $.each(result.items, function (i, item) {
-//         $("<img>").attr("src", item.media.m).appendTo("#outputDiv").addClass('image-flick');
-//         if (i === 5) { 
-//             return false;
-//         }
-//     });
-
-// });
 
 function JavaScriptFetch() {
     var script = document.createElement('script');
     script.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + document.getElementById("search").value;
     document.querySelector('head').appendChild(script);
-    // $.getJSON(flickerAPI, {
-    //     tags: $("#search").val(),
-    //     tagmode: "any",
-    //     format: "json"
-    // }).done(function (result, status, xhr) {
-    //     $.each(result.items, function (i, item) {
-    //         $("<img>").attr("src", item.media.m).appendTo("#outputDiv").addClass('image-flick');
-    //         if (i === 5) { 
-    //             return false;
-    //         }
-    //     });
-    
-    // });
 }
+
 function jsonFlickrFeed(data) {
     var image = "";
     data.items.forEach(function (element) {
         image += "<img src=\"" + element.media.m + "\"/>";
     });
-    document.getElementById("outputDiv").innerHTML = image;
+    document.getElementById("outPutDiv").innerHTML = image;
 }
 
-// To make the search button work upon pressing enter: //
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------  Keyboard inputs ----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+// Connects the search button to 'ENTER' on your keyboard
 var input = document.getElementById("search");
 
 input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {                     // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {                     // Number 13 = 'ENTER' key on the keyboard
     event.preventDefault();
     document.getElementById("submit").click();
   }
 });
+
